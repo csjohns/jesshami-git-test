71cacb7fa4f3ac4075a1fb8b4425c64970281284
Carolina Johnson <csjohns@uw.edu>
2016-06-16 10:44:52 -0700

    Added python test file test.py with fib() function

---
+++ test.py
@@ -0,0 +1,14 @@
+def print_hello_world():
+	print('hello world!')
+
+def fib(n):
+	fibs = []
+	a, b = 0, 1
+	for i in range(n):
+		a, b = b, a + b
+		fibs.append(a)
+	return fibs
+
+print(list(fib(10)))
+
+print_hello_world()
